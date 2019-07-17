c20eaf83f26620ca14c6414ff7e0538c6c3f2905
Tess Meyer <tessoliviameyer@gmail.com>
2019-07-18 09:59:32 +1200

    add splash page

---
+++ client/components/Splash.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+class App extends React.Component {
+    constructor(props) {
+        super(props)
+        
+        this.state = {
+
+        }
+    }
+
+
+    render() { 
+        return (  
+            <React.Fragment>
+                <h1 className="tracking-in-expand middle">Festival Attendee Mapper</h1>
+            </React.Fragment>
+        );
+    }
+}
+ 
+export default App;

c87465253975bb03c1b0480c8e16f3961570603d
daniel-vanammers <dvanammers@gmail.com>
2019-07-18 10:05:05 +1200

    Splash Page Component skeleton complete

---
+++ client/components/Logo.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Logo extends React.Component {
+    state = {  }
+    render() { 
+        return ( 
+            <>
+            <h2>I am a Logo Component</h2>
+            </>
+         );
+    }
+}
+ 
+export default Logo;

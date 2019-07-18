68bb0db8c91fd70a02a12ae7967116bac8efd168
ben-sherwood <ben.sherwood0@gmail.com>
2019-07-19 09:51:45 +1200

    userId lib imported and implemented

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,4 +1,6 @@
 import React from 'react'
+const uuidv4 = require('uuid/v4')
+
 import Splash from './Splash'
 // import Test from './test'
 import Footer from './Footer'
@@ -12,12 +14,20 @@ class App extends React.Component {
         super(props)
 
         this.state = {
-            
+
         }
     }
 
-    componentDidMount(){
-        this.geoLocate() 
+    componentDidMount() {
+        let userStorage = window.localStorage;
+        if (userStorage.userId){
+            console.log("Existing user found: " + userStorage.userId)
+        }else{
+            userStorage.userId = uuidv4()
+            console.log("New User Set: " + userStorage.userId)
+        }
+        this.geoLocate()
+
     }
 
 
@@ -25,32 +35,34 @@ class App extends React.Component {
     geoLocate = () => {
         const error = () => {
             console.warn(`ERROR(${err.code}): ${err.message}`);
-          }
+        }
         const options = {
             enableHighAccuracy: true,
             timeout: 60000,
             maximumAge: 0
-          };
-        setInterval(function(){
+        };
+        setInterval(function () {
             navigator.geolocation.getCurrentPosition(this.saveLocation, error, options)
         }.bind(this), 3000)
     }
 
-    saveLocation =(pos) => {
+    saveLocation = (pos) => {
         let crd = pos.coords;
-        const locationTag ={}
-    
+        const locationTag = {}
 
-       if(this.outOfBoundsChecker(crd.latitude, crd.longitude)){
-        locationTag.latitude = crd.latitude
-        locationTag.longitude = crd.longitude
-        locationTag.accuracy = crd.accuracy
-        
-        this.setState({
-            geoTags: locationTag
-        })
-        addGeoLocationApi(this.state.geoTags)
-       }
+
+        if (this.outOfBoundsChecker(crd.latitude, crd.longitude)) {
+            locationTag.latitude = crd.latitude
+            locationTag.longitude = crd.longitude
+            locationTag.accuracy = crd.accuracy
+            locationTag.user  = window.localStorage.userId
+
+            this.setState({
+                geoTags: locationTag
+            })
+            addGeoLocationApi(this.state.geoTags)
+        }
+        console.log("Out of bounds!")
     }
 
     outOfBoundsChecker = (lat, long) => {
@@ -58,24 +70,24 @@ class App extends React.Component {
         const westLong = 174.772497
         const northLat = -41.290972
         const southLat = -41.297387
-    
-    
-        if(lat >= southLat && lat <= northLat && long <= eastLong && long >= westLong){
+
+
+        if (lat >= southLat && lat <= northLat && long <= eastLong && long >= westLong) {
             return true
         }
         return false
     }
 
-    render() { 
-        return (   
+    render() {
+        return (
             <React.Fragment>
                 <div>
-                <Splash/>
+                    <Splash />
                     <div className='content'>
                         {/* <Test/> */}
                         <ApiTest />
-                        <Footer/>
-                
+                        <Footer />
+
                     </div>
                 </div>
             </React.Fragment>

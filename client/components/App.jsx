1c0dc0c6d1867c8b9a8356ba1e942855ed660519
Hanh <hanh@Sarahs-MBP-2.localdomain>
2019-07-19 10:37:41 +1200

    add map to page

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Splash from './Splash'
 // import Test from './test'
 import Footer from './Footer'
+import SimpleExample from './Map'
 
 import { addGeoLocationApi } from '../api/geoLocationApi';
 import ApiTest from './ApiTest';
@@ -74,6 +75,7 @@ class App extends React.Component {
                     <div className='content'>
                         {/* <Test/> */}
                         <ApiTest />
+                        <SimpleExample/>
                         <Footer/>
                 
                     </div>
--- client/components/Map.jsx
+++ client/components/Map.jsx
@@ -1,25 +1,68 @@
 import React from 'react'
+import { render } from 'react-dom'
+import { Map as LeafletMap, Marker, Popup, TileLayer } from 'react-leaflet'
 
 
-
-class Map extends React.Component {
-    constructor(props) {
-        super(props)
+// class Map extends React.Component {
+//     constructor(props) {
+//         super(props)
         
-        this.state = {
+//         this.state = {
 
-        }
+//         }
+//     }
+
+
+//     render() { 
+//         return (  
+//             <>
+//                 <div id='map'>
+//                   <p>
+//                       map
+//                   </p>
+
+//                 </div>
+//             </>
+//         );
+//     }
+// }
+
+// const { Map: LeafletMap, TileLayer, Marker, Popup } = ReactLeaflet
+
+class SimpleExample extends React.Component {
+  constructor() {
+    super()
+
+    this.state = {
+      lat: -41.290972,
+      lng: 174.780310,
+      zoom: 13
     }
+  }
 
-
-    render() { 
-        return (  
-            <React.Fragment>
-                <h2>Hi I'm a map!!</h2>
-            </React.Fragment>
-        );
-    }
+  render() {
+    const position = [this.state.lat, this.state.lng];
+    return (
+      <LeafletMap center={position} zoom={this.state.zoom}>
+        <TileLayer
+          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+          url='https://{s}.tile.osm.org/{z}/{x}/{y}.png'
+        />
+        <Marker position={position}>
+          <Popup>
+            A pretty CSS3 popup. 
+            <br/> 
+            Easily customizable.
+          </Popup>
+        </Marker>
+      </LeafletMap>
+    );
+  }
 }
+
+
+// ReactDOM.render(<SimpleExample/>, document.getElementById('container'))
+  
  
-export default Map;
+export default SimpleExample;
 

5f5b76b4a352f4b26e6a9d88a8fa689ac6108595
pano-skylakis <pano_skylakis@outlook.com>
2019-07-19 19:52:44 +1200

    add seed data and adjust migration columns

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -3,7 +3,7 @@ const uuidv4 = require('uuid/v4')
 
 import Splash from './Splash'
 import Footer from './Footer'
-import Map from './Map'
+import PrimaryMap from './Map'
 import BarGraph from './BarGraph'
 import LineGraph from './LineGraph'
 
@@ -20,82 +20,83 @@ class App extends React.Component {
     }
 
     componentDidMount() {
-        let userStorage = window.localStorage;
-        if (userStorage.userId){
-            console.log("Existing user found: " + userStorage.userId)
-        }else{
-            userStorage.userId = uuidv4()
-            console.log("New User Set: " + userStorage.userId)
-        }
-        this.geoLocate()
+        // let userStorage = window.localStorage;
+        // if (userStorage.userId){
+        //     console.log("Existing user found: " + userStorage.userId)
+        // }else{
+        //     userStorage.userId = uuidv4()
+        //     console.log("New User Set: " + userStorage.userId)
+        // }
+        // this.geoLocate()
         this.getLocations()
     }
 
 
         // Get Locations from Database
     getLocations = () => {
-            getGeoLocationsApi()
-                .then(locations => {
-                    this.refreshLocations(locations)
-                })
+        getGeoLocationsApi()
+            .then(locations => {
+                this.refreshLocations(locations)
+            })
         }
     
-        refreshLocations = (locations) => {
-            this.setState({
-                locs: locations || []
-            })
-            console.log(this.state.locs)
-        }
+    refreshLocations = (locations) => {
+        this.setState({
+            locs: locations || []
+        })
+        console.log(this.state.locs)
+    }
 
 
 
         // Track Locations
-    geoLocate = () => {
-        const error = () => {
-            console.warn(`ERROR(${err.code}): ${err.message}`);
-        }
-        const options = {
-            enableHighAccuracy: true,
-            timeout: 60000,
-            maximumAge: 0
-        };
-        setInterval(function () {
-            navigator.geolocation.getCurrentPosition(this.saveLocation, error, options)
-        }.bind(this), 3000)
-    }
+    // geoLocate = () => {
+    //     const error = () => {
+    //         console.warn(`ERROR(${err.code}): ${err.message}`);
+    //     }
+    //     const options = {
+    //         enableHighAccuracy: true,
+    //         timeout: 60000,
+    //         maximumAge: 0
+    //     };
+    //     let interval = setInterval(function () {
+    //         navigator.geolocation.getCurrentPosition(this.saveLocation, error, options)
+    //     }.bind(this), 3000)
+    // }
 
-    saveLocation = (pos) => {
-        let crd = pos.coords;
-        const locationTag = {}
+    // saveLocation = (pos) => {
+    //     let crd = pos.coords;
+    //     const locationTag = {}
 
     
-        if (this.outOfBoundsChecker(crd.latitude, crd.longitude)) {
-            locationTag.latitude = crd.latitude
-            locationTag.longitude = crd.longitude
-            locationTag.accuracy = crd.accuracy
-            locationTag.user  = window.localStorage.userId
+    //     if (this.outOfBoundsChecker(crd.latitude, crd.longitude)) {
+    //         locationTag.latitude = crd.latitude
+    //         locationTag.longitude = crd.longitude
+    //         locationTag.accuracy = crd.accuracy
+    //         locationTag.user  = window.localStorage.userId
+    //         locationTag.timestamp = Date.now()
 
-            this.setState({
-                geoTags: locationTag
-            })
-            addGeoLocationApi(this.state.geoTags)
-            this.getLocations()
-        }
-        console.log("Out of bounds!")
-    }
+    //         this.setState({
+    //             geoTags: locationTag
+    //         })
+    //         addGeoLocationApi(this.state.geoTags)
+    //         this.getLocations()
+    //     }
+    //     console.log("Out of bounds!")
+    // }
 
-    outOfBoundsChecker = (lat, long) => {
-        const eastLong = 174.780310
-        const westLong = 174.772497
-        const northLat = -41.290972
-        const southLat = -41.297387
+    // outOfBoundsChecker = (lat, long) => {
+    //     const eastLong = 174.780310
+    //     const westLong = 174.772497
+    //     const northLat = -41.290972
+    //     const southLat = -41.297387
 
 
-        if (lat >= southLat && lat <= northLat && long <= eastLong && long >= westLong) {
-            return true
-        }
-        return false
-    }
+    //     if (lat >= southLat && lat <= northLat && long <= eastLong && long >= westLong) {
+    //         return true
+    //     }
+    //     return false
+    // }
 
     render() {
         return (
@@ -103,7 +104,7 @@ class App extends React.Component {
                 <div>
                     <Splash />
                     <div className='content'>
-                        <Map/> 
+                        <PrimaryMap/> 
                         <div className="graph-container">   
                             <BarGraph />
                             <LineGraph />
--- seeds/geolocation.js
+++ seeds/geolocation.js
@@ -5,10 +5,55 @@ exports.seed = function(knex) {
     .then(function () {
       // Inserts seed entries
       return knex('geolocation').insert([
-        {id: 1, latitude: '42.4535', longitude: '174.3434', user:'goku'},
-        {id: 2, latitude: '42.5435', longitude: '174.2456', user:'vegeta'},
-        {id: 3, latitude: '42.5644', longitude: '173.4343', user:'gohan'},
-        {id: 4, latitude: '41.3433', longitude: '173.3433', user:'trunks'}
+        {id: 1, latitude: '42.4535', longitude: '174.3434', timestamp:1563544800, user:'goku'},
+        {id: 2, latitude: '42.5435', longitude: '174.2456', timestamp:1563545100, user:'vegeta'},
+        {id: 3, latitude: '42.5644', longitude: '173.4343', timestamp:1563545400, user:'gohan'},
+        {id: 4, latitude: '41.3433', longitude: '173.3433', timestamp:1563545700, user:'trunks'},
+        {id: 5, latitude: '41.3433', longitude: '173.3433', timestamp:1563546000, user:'trunks'},
+        {id: 6, latitude: '41.3433', longitude: '173.3433', timestamp:1563546300, user:'trunks'},
+        {id: 7, latitude: '41.3433', longitude: '173.3433', timestamp:1563546600, user:'trunks'},
+        {id: 8, latitude: '41.3433', longitude: '173.3433', timestamp:1563546900, user:'trunks'},
+        {id: 9, latitude: '41.3433', longitude: '173.3433', timestamp:1563547200, user:'trunks'},
+        {id: 10, latitude: '41.3433', longitude: '173.3433', timestamp:1563547500, user:'trunks'},
+        {id: 11, latitude: '41.3433', longitude: '173.3433', timestamp:1563547800, user:'trunks'},
+        {id: 12, latitude: '41.3433', longitude: '173.3433', timestamp:1563548100, user:'trunks'},
+        {id: 13, latitude: '41.3433', longitude: '173.3433', timestamp:1563548400, user:'trunks'},
+        {id: 14, latitude: '41.3433', longitude: '173.3433', timestamp:1563549000, user:'trunks'},
+        {id: 15, latitude: '41.3433', longitude: '173.3433', timestamp:1563549300, user:'trunks'},
+        {id: 16, latitude: '41.3433', longitude: '173.3433', timestamp:1563549600, user:'trunks'},
+        {id: 17, latitude: '41.3433', longitude: '173.3433', timestamp:1563549900, user:'trunks'},
+        {id: 18, latitude: '41.3433', longitude: '173.3433', timestamp:1563550200, user:'trunks'},
+        {id: 19, latitude: '41.3433', longitude: '173.3433', timestamp:1563550500, user:'trunks'},
+        {id: 20, latitude: '41.3433', longitude: '173.3433', timestamp:1563550800, user:'trunks'},
+        {id: 21, latitude: '41.3433', longitude: '173.3433', timestamp:1563551100, user:'trunks'},
+        {id: 22, latitude: '41.3433', longitude: '173.3433', timestamp:1563551400, user:'trunks'},
+        {id: 23, latitude: '41.3433', longitude: '173.3433', timestamp:1563551700, user:'trunks'},
+        {id: 24, latitude: '41.3433', longitude: '173.3433', timestamp:1563552000, user:'trunks'}
       ]);
     });
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

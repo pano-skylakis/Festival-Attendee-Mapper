818cfc27ecc9eabd45501892657d0f40a8e35995
Hanh <hanh@Sarahs-MBP-2.localdomain>
2019-07-21 11:34:19 +1200

    add marker

--- client/components/Map.jsx
+++ client/components/Map.jsx
@@ -1,5 +1,16 @@
 import React from 'react'
-import { Map as LeafletMap, TileLayer } from 'react-leaflet'
+import { Map as LeafletMap, TileLayer, Marker, Popup } from 'react-leaflet'
+
+// const pointerIcon = new L.Icon({
+//   iconUrl: require(''),
+//   iconRetinaUrl: require(''),
+//   iconAnchor: [5, 55],
+//   popupAnchor: [10, -44],
+//   iconSize: [25, 55],
+//   shadowUrl: '',
+//   shadowSize: [68, 95],
+//   shadowAnchor: [20, 92],
+// })
 
 class Map extends React.Component {
   constructor() {
@@ -8,20 +19,37 @@ class Map extends React.Component {
     this.state = {
       lat: -41.293699,
       lng: 174.775497,
-      zoom: 16
+      zoom: 16,
+      markers: []
     }
+  }
+
+  addMarker = (e) => {
+    const {markers} = this.state
+    markers.push(e.latlng)
+    this.setState({markers})
   }
 
   render() {
     const position = [this.state.lat, this.state.lng];
     return (
-      <LeafletMap className="map-margin" center={position} zoom={this.state.zoom}>
+      <LeafletMap className="map-margin" center={position} zoom={this.state.zoom} onClick={this.addMarker}>
         <TileLayer url='https://{s}.tile.osm.org/{z}/{x}/{y}.png'/>
+        {this.state.markers.map((position, idx) => 
+          <Marker key={`marker-${idx}`} position={position}>
+          <Popup>
+            <span>Info</span>
+          </Popup>
+        </Marker>
+        )}
       </LeafletMap>
     );
   }
 }
   
 
-export default Map;
 
+
+
+
+export default Map

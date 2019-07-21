035fb9190220f5834c75f05ea1853217243b68ed
pano-skylakis <pano_skylakis@outlook.com>
2019-07-22 09:53:03 +1200

    slider now displays current value

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -136,6 +136,7 @@ class App extends React.Component {
           >
             <input type="date" onChange={this.handleDateChange} />
             <div className="slidecontainer">
+              <p>{Number(this.state.sliderValue) < 10 ? `0${this.state.sliderValue}:00` : `${this.state.sliderValue}:00` }</p>
               <input
                 type="range"
                 min="0"

70dd957495f88e6393a79f397376f165467357b3
pano-skylakis <pano_skylakis@outlook.com>
2019-07-22 20:33:37 +1200

    refactored geoLocation to userLanding component

--- client/components/Dashboard.jsx
+++ client/components/Dashboard.jsx
@@ -11,12 +11,12 @@ import LineGraph from "./LineGraph";
 import Stats from "./Stats";
 
 import {
-  addGeoLocationApi,
   getGeoLocationsApi,
   getGeoLocationByTimeApi,
   getHeatMapValues,
   getHeatMapIntensity
 } from "../api/geoLocationApi";
+
 
 class Dashboard extends React.Component {
   constructor(props) {
@@ -34,117 +34,55 @@ class Dashboard extends React.Component {
   
     }
   
-    componentDidMount() {
+  componentDidMount() {
   
-      // gets unique heatmap values + intensities.
-      getHeatMapValues()
-      .then(res=>{
-        Promise.all(res.map(getHeatMapIntensity)).then(info => {
-          this.setState({
-            heatmapData: info
-          })
+    // gets unique heatmap values + intensities.
+    getHeatMapValues()
+    .then(res=>{
+      Promise.all(res.map(getHeatMapIntensity)).then(info => {
+        this.setState({
+          heatmapData: info
         })
       })
-  
-      //Assigns each user a unique id
-      let userStorage = window.localStorage;
-      if (userStorage.userId){
-          console.log("Existing user found: " + userStorage.userId)
-      }else{
-          userStorage.userId = uuidv4()
-      }
-      this.geoLocate()
-      this.getLocations();
-    }
-    
-    // Get Locations from Database
-    getLocations = () => {
-      getGeoLocationsApi().then(locations => {
-        this.refreshLocations(locations);
-      });
-    };
-  
-    refreshLocations = locations => {
-      this.setState({
-        locs: locations || []
-      });
-    };
-  
-    // Track Locations
-    geoLocate = () => {
-      const error = () => {
-        console.warn(`ERROR(${err.code}): ${err.message}`);
-      }
-      const options = {
-        enableHighAccuracy: true,
-        timeout: 5000,
-        maximumAge: 0
-      };
-      let interval = setInterval(function () {
-        navigator.geolocation.getCurrentPosition(this.saveLocation, error, options)
-      }.bind(this), 3000)
-    }
-  
-    saveLocation = (pos) => {
-      let crd = pos.coords;
-      const locationTag = {}
-  
-      if (this.outOfBoundsChecker(crd.latitude, crd.longitude)) {
-        locationTag.latitude = crd.latitude
-        locationTag.longitude = crd.longitude
-        locationTag.accuracy = crd.accuracy
-        locationTag.user = window.localStorage.userId
-        locationTag.timestamp = Date.now()
-  
-  
-        this.setState({
-          geoTags: locationTag
-        })
-        addGeoLocationApi(locationTag)
-        this.getLocations()
-      } else {
-        console.log("Out of bounds!")
-      }
-    }
-    outOfBoundsChecker = (lat, long) => {
-      const eastLong = 174.779602
-      const westLong = 174.771753
-      const northLat = -41.288876
-      const southLat = -41.297850
-  
-      if (lat >= southLat && lat <= northLat && long <= eastLong && long >= westLong) {
-        return true
-      }
-      return false
-    }
+    })
 
-    handleClick = () => {
-        this.state.barGraph ? this.setState({ barGraph: false, lineGraph: true}) : this.setState({ barGraph: true, lineGraph: false})
-    }
+    this.getLocations();
+  }
+    
+  // Get Locations from Database
+  getLocations = () => {
+    getGeoLocationsApi().then(locations => {
+      this.refreshLocations(locations);
+    });
+  };
+
+  refreshLocations = locations => {
+    this.setState({
+      locs: locations || []
+    });
+  };
+
   
-    handleDateChange = e => {
-      this.setState({ currentDate: e.target.value });
-    };
 
-    outOfBoundsChecker = (lat, long) => {
-        const eastLong = 174.779602
-        const westLong = 174.771753
-        const northLat = -41.288876
-        const southLat = -41.297850
-    }
+  handleClick = () => {
+      this.state.barGraph ? this.setState({ barGraph: false, lineGraph: true}) : this.setState({ barGraph: true, lineGraph: false})
+  }
 
-    handleSliderChange = e => {
-        // 2019-07-20T11:06:55+0000  <<< this is the format the date must be in (ISO8106)
-        let date = "";
-    
-        this.setState({ sliderValue: e.target.value });
-    
-        Number(this.state.sliderValue) < 10 ? (date = `${this.state.currentDate}T0${this.state.sliderValue}:00:55+0000`) : (date = `${this.state.currentDate}T${this.state.sliderValue}:00:55+0000`);
-        
-        let unixTimestamp = moment(`${date}`).unix();
-    
-        getGeoLocationByTimeApi(unixTimestamp, unixTimestamp + 3601)
-      };
+  handleDateChange = e => {
+    this.setState({ currentDate: e.target.value });
+  };
+
+
+  handleSliderChange = e => {
+      // 2019-07-20T11:06:55+0000  <<< this is the format the date must be in (ISO8106)
+      let date = "";
+  
+      this.setState({ sliderValue: e.target.value });
+      Number(this.state.sliderValue) < 10 ? (date = `${this.state.currentDate}T0${this.state.sliderValue}:00:55+0000`) : (date = `${this.state.currentDate}T${this.state.sliderValue}:00:55+0000`);
+      
+      let unixTimestamp = moment(`${date}`).unix();
+      getGeoLocationByTimeApi(unixTimestamp, unixTimestamp + 3601)
+  };
 
   render() {
     return (
--- client/components/UserLanding.jsx
+++ client/components/UserLanding.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { addGeoLocationApi } from '../api/geoLocationApi'
 
 class UserLanding extends React.Component {
     constructor(props) {
@@ -7,6 +8,68 @@ class UserLanding extends React.Component {
         this.state = {
 
         }
+    }
+
+    componentDidMount() {
+        //Assigns each user a unique id
+        let userStorage = window.localStorage;
+        if (userStorage.userId){
+            console.log("Existing user found: " + userStorage.userId)
+        }else{
+            userStorage.userId = uuidv4()
+        }
+        this.geoLocate()
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.interval)
+    }
+    
+    // Track Locations
+    geoLocate = () => {
+        const error = () => {
+          console.warn(`ERROR(${err.code}): ${err.message}`);
+        }
+        const options = {
+          enableHighAccuracy: true,
+          timeout: 5000,
+          maximumAge: 0
+        };
+        this.interval = setInterval(function () {
+          navigator.geolocation.getCurrentPosition(this.saveLocation, error, options)
+        }.bind(this), 5000)
+    }
+    
+    saveLocation = (pos) => {
+    let crd = pos.coords;
+    const locationTag = {}
+
+    if (this.outOfBoundsChecker(crd.latitude, crd.longitude)) {
+            locationTag.latitude = crd.latitude
+            locationTag.longitude = crd.longitude
+            locationTag.accuracy = crd.accuracy
+            locationTag.user = window.localStorage.userId
+            locationTag.timestamp = Date.now()
+
+            this.setState({
+            geoTags: locationTag
+            })
+            addGeoLocationApi(locationTag)
+
+        } else {
+            console.log("Out of bounds!")
+        }
+    }
+    outOfBoundsChecker = (lat, long) => {
+    const eastLong = 174.779602
+    const westLong = 174.771753
+    const northLat = -41.288876
+    const southLat = -41.297850
+
+    if (lat >= southLat && lat <= northLat && long <= eastLong && long >= westLong) {
+        return true
+    }
+    return false
     }
 
 

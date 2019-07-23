598e5fc6ce37529421efa5fdd569522541239dbb
pano-skylakis <pano_skylakis@outlook.com>
2019-07-24 09:30:00 +1200

    add default marker to fix crash on initial map click

--- client/components/MapIcons.jsx
+++ client/components/MapIcons.jsx
@@ -72,19 +72,13 @@ export default {
     iconSize: [15, 35],
     iconAnchor: [9, 25],
     popupAnchor: [10, -44],
+  }),
+
+
+  "defaultMarkerIcon": new L.icon({
+    iconUrl: './images/defaultMarker.png',
+    iconSize: [18, 35],
+    iconAnchor: [9, 25],
+    popupAnchor: [10, -44]
   })
 }
-  
-  // L.marker([50.505, 30.57], {icon: toiletIcon}).addTo(map);
-  
-  // const pointerIcon = new L.Icon({
-  //   iconUrl: ‘./images/icon.svg’,
-  //   iconRetinaUrl: ‘./images/icon.svg’,
-  //   iconAnchor: [5, 55],
-  //   popupAnchor: [10, -44],
-  //   iconSize: [25, 55],
-  //   shadowUrl: ‘./images/marker.png’,
-  //   shadowSize: [68, 95],
-  //   shadowAnchor: [20, 92],
-  //  })
-  

4edc7e73b0d6ade687f6607307260d30e5a2ac41
pano-skylakis <pano_skylakis@outlook.com>
2019-07-24 14:05:23 +1200

    comment out remove marker script

--- server/db/markerLocations.js
+++ server/db/markerLocations.js
@@ -1,15 +1,15 @@
 const connection = require('./connection')
 
-//deletes all markers from database
-function deleteAllMarkers(db = connection) {
-    console.log('yep')
-    return db('marker_locations').where('id', '>', 0).del()
-        .then(deleted => {
-            console.log(deleted)
-        })
-}
+// uncomment to delete all markers from database on deploy
+// function deleteAllMarkers(db = connection) {
+//     console.log('yep')
+//     return db('marker_locations').where('id', '>', 0).del()
+//         .then(deleted => {
+//             console.log(deleted)
+//         })
+// }
 
-deleteAllMarkers()
+// deleteAllMarkers()
 
 
 //marker functions

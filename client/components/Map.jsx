840483286bd5454af0da359a63944e9b7aecb826
ben-sherwood <ben.sherwood0@gmail.com>
2019-07-24 13:28:34 +1200

    Slider now displays all map data if dragged to position 0

--- client/components/Map.jsx
+++ client/components/Map.jsx
@@ -142,7 +142,7 @@ class Map extends React.Component {
     return (
       <React.Fragment>
         <LeafletMap oncontextmenu={this.addPolyPosition} className="map-margin"  center={centerPosition} zoom={this.state.zoom} fitBoundsOnLoad={this.state.positions} onClick={this.addMarker} maxZoom={this.state.maxZoom}>      
-          <Polygon color="black" positions = {this.state.positions}/>
+          
 
 
               {this.state.savedMarkers.map((position, idx) => {
@@ -161,6 +161,7 @@ class Map extends React.Component {
                 {/* map layer-control */}
           <LayersControl position='topright'>
             <LayersControl.BaseLayer checked name='Street View'>
+            <Polygon color="black" positions = {this.state.positions}/>
               <TileLayer url='https://{s}.tile.osm.org/{z}/{x}/{y}.png' />
             </LayersControl.BaseLayer>
             <LayersControl.BaseLayer name='Satellite'>

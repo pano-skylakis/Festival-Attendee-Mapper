7870e0c633f5a7106c097270ae2100572728b8b6
pano-skylakis <pano_skylakis@outlook.com>
2019-08-09 11:49:10 +1200

    add links to github in footer

--- client/components/Footer.jsx
+++ client/components/Footer.jsx
@@ -6,8 +6,8 @@ class Footer extends React.Component {
             <div className='footer'>
                 <p>Bought to you by Festival Attendee Mapper™</p>
                 <p>We make sick apps</p>
-                <p>Painstakingly crafted by Daniel van Ammers, Daniel Bridewell, 
-                    Pano Skylakis, Ben Sherwood, Hanh Phan, Quentin Papera and Tess Meyer</p>
+                <p>Painstakingly crafted by <a href="https://github.com/daniel-vanammers">Daniel van Ammers</a>, <a href="">Daniel Bridewell</a>, 
+                    <a href="https://github.com/pano-skylakis"> Pano Skylakis</a>, <a href="https://github.com/ben-sherwood">Ben Sherwood</a>, <a href="https://github.com/tess-meyer">Tess Meyer</a>, <a href="">Hanh Phan</a> and <a href="https://github.com/quentin-papera">Quentin Papera</a></p>
             </div>
          );
     }
